5cfc79385cb88b949201e522fac44ad9b2f116d0
KartheekGurram21 <techloom.web3@gmail.com>
2025-07-08 22:32:39 +0530

    bg-color-changer

---
+++ web-development/week-3/easy/bg-color-changer/script.js
@@ -0,0 +1,27 @@
+document.getElementById('red').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'red';
+});
+
+document.getElementById('yellow').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'yellow';
+});
+
+document.getElementById('black').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'black';
+});
+
+document.getElementById('purple').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'purple';
+});
+
+document.getElementById('green').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'green';
+});
+
+document.getElementById('blue').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'blue';
+});
+
+document.getElementById('default').addEventListener('click', async () => {
+    document.querySelector('body').style.backgroundColor = 'orange';
+});
